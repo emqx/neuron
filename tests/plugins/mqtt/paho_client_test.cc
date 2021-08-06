e8b8e52d6be36845398c637912235281d225bc2b
gc87 <sfme@qq.com>
2021-08-06 10:22:27 +0800

    Add test fixture to mqtt plugin.

--- tests/plugins/mqtt/paho_client_test.cc
+++ tests/plugins/mqtt/paho_client_test.cc
@@ -7,25 +7,39 @@
 
 using namespace std;
 
-TEST(PahoClientCase, paho_client_open)
-{
-    string clientID("neuron-lite-mqtt-plugin");
-    string topic("MQTT Examples");
-    string connection("tcp://");
-    string host("192.168.50.165");
-    string port("1883");
+class PahoClientTest : public ::testing::Test {
+protected:
+    PahoClientTest() { }
+    ~PahoClientTest() override { }
 
-    option_t option;
-    option.clientid           = (char *) clientID.c_str();
-    option.MQTT_version       = 4;
-    option.topic              = (char *) topic.c_str();
-    option.qos                = 1;
-    option.connection         = (char *) connection.c_str();
-    option.host               = (char *) host.c_str();
-    option.port               = (char *) port.c_str();
-    option.keepalive_interval = 20;
-    option.clean_session      = 1;
+    void SetUp() override
+    {
+        string clientID("neuron-lite-mqtt-plugin");
+        string topic("MQTT Examples");
+        string connection("tcp://");
+        string host("192.168.50.165");
+        string port("1883");
 
+        option.clientid           = (char *) clientID.c_str();
+        option.MQTT_version       = 4;
+        option.topic              = (char *) topic.c_str();
+        option.qos                = 1;
+        option.connection         = (char *) connection.c_str();
+        option.host               = (char *) host.c_str();
+        option.port               = (char *) port.c_str();
+        option.keepalive_interval = 20;
+        option.clean_session      = 1;
+    }
+
+    void TearDown() override { }
+
+    option_t       option;
+    option_t       option1;
+    option_t       option2;
     paho_client_t *paho;
+};
+
+TEST_F(PahoClientTest, paho_client_open)
+{
     EXPECT_EQ(paho_client_open(&option, &paho), 0);
 }
