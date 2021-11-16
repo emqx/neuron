3387c82c7562e52f8326b021fb63237e6bd75c9c
alvin1221 <alvin@emqx.io>
2021-11-16 16:14:23 +0800

    Adapt cross compile

--- install-libs.sh
+++ install-libs.sh
@@ -1,44 +1,58 @@
 #!/bin/bash
 
-system=`uname`
+system=$(uname)
 echo "System: $system"
 
-if [ $system == 'Linux' ];then
-    if [ `whoami` != "root" ];then
+if [ $system == 'Linux' ]; then
+    if [ $(whoami) != "root" ]; then
         echo "Please use root access"
-    exit 2
+        exit 2
     fi
 fi
 
-lib_list=(nng jansson jwt MQTT-C openssl gtest open62541 yaml)
+lib_list=(zlib openssl nng jansson jwt MQTT-C gtest open62541 yaml)
 
 list_str=""
-for var in ${lib_list[*]};do
+for var in ${lib_list[*]}; do
     list_str+=$var" "
 done
+
+arch=x86_64
+is_build_all=FALSE
+pkgname=
+is_cross=FALSE
+compiler_opt=
+install_opt=
+cross_compiler_prefix=
+ssl_lib_flag=
+git_url_prefix="https://github.com/"
 
 function usage() {
     echo "Usage: $0 "
     echo "  -a  Build all dependencies"
-    echo "  -i  Specify installation options (opts: $list_str)"
+    echo "  -n  Specify dependence name (opts: $list_str)"
+    echo "  -c  Cross compiler mode (default: FALSE)"
+    echo "  -p  Cross compiler prefix (opts: x86_64-linux-gnu, arm-linux-gnueabihf, aarch64-linux-gnu, ...)"
+    echo "  -s  System architecture (only for openssl, opts: x86_64, armv4, aarch64, ...) "
+    echo "  -r  Use github repository SSH url (default: HTTPS url)"
 }
 
-if [ $# -eq 0 ];then
-usage
-exit
+if [ $# -eq 0 ]; then
+    usage
+    exit
 fi
 
 # nng
-function build_nng()
-{
+function build_nng() {
     echo "Building nng (v1.5.2)"
-    if [ ! -d nng ];then
-        git clone -b v1.5.2 https://github.com/nanomsg/nng.git
+    if [ ! -d nng ]; then
+        git clone -b v1.5.2 ${git_url_prefix}nanomsg/nng.git
     fi
     cd nng
+    rm -rf build
     mkdir build
     cd build
-    cmake -G Ninja -DBUILD_SHARED_LIBS=1 ..
+    cmake -G Ninja -DBUILD_SHARED_LIBS=OFF -DNNG_TESTS=OFF ${compiler_opt} ${install_opt} ..
     ninja
     ninja install
 
@@ -47,17 +61,18 @@ function build_nng()
 }
 
 # jansson
-function build_jansson()
-{
+function build_jansson() {
     echo "Building jansson"
-    if [ ! -d jansson ];then
-        git clone https://github.com/akheron/jansson.git
+    if [ ! -d jansson ]; then
+        git clone ${git_url_prefix}akheron/jansson.git
     fi
     cd jansson
     rm -rf build
     mkdir build
     cd build
-    cmake -G Ninja -DJANSSON_BUILD_DOCS=OFF ..
+    cmake -G Ninja -DJANSSON_BUILD_DOCS=OFF \
+        -DJANSSON_EXAMPLES=OFF \
+        ${compiler_opt} ${install_opt} ..
     ninja
     ninja install
 
@@ -66,17 +81,16 @@ function build_jansson()
 }
 
 # jwt
-function build_jwt()
-{
+function build_jwt() {
     echo "Building libjwt"
-    if [ ! -d libjwt ];then
-        git clone https://github.com/benmcollins/libjwt.git
+    if [ ! -d libjwt ]; then
+        git clone ${git_url_prefix}benmcollins/libjwt.git
     fi
     cd libjwt
     rm -rf build
     mkdir build
     cd build
-    cmake -G Ninja -DBUILD_EXAMPLES=OFF $ssl_lib_flag  ..
+    cmake -G Ninja -DBUILD_EXAMPLES=OFF ${ssl_lib_flag} ${compiler_opt} ${install_opt} ..
     ninja
     ninja install
 
@@ -85,86 +99,140 @@ function build_jwt()
 }
 
 # MQTT-C
-function build_MQTT-C()
-{
+function build_MQTT-C() {
     echo "Building MQTT-C (1.1.5)"
-    if [ ! -d MQTT-C ];then
-        git clone -b 1.1.5 https://github.com/LiamBindle/MQTT-C.git
+    if [ ! -d MQTT-C ]; then
+        git clone -b 1.1.5 ${git_url_prefix}neugates/MQTT-C.git
     fi
     cd MQTT-C
     rm -rf build
     mkdir build
     cd build
-    cmake -G Ninja -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DMQTT_C_OpenSSL_SUPPORT=ON ..
+    cmake -G Ninja -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+        -DMQTT_C_OpenSSL_SUPPORT=ON \
+        -DMQTT_C_EXAMPLES=OFF \
+        ${ssl_lib_flag} ${compiler_opt} ${install_opt} ..
     ninja
     ninja install
     echo "Leaving MQTT-C "
     cd ../../
 }
 
-function build_yaml()
-{   
-    git clone https://github.com/yaml/libyaml.git
+function build_yaml() {
+    echo "Building yaml"
+    git clone ${git_url_prefix}yaml/libyaml.git
     cd libyaml
     rm -rf build
-    mkdir build && cd build && cmake DBUILD_SHARED_LIBS=1 .. && make
-    make install
+    mkdir build
+    cd build
+    cmake -G Ninja -DBUILD_SHARED_LIBS=ON ${compiler_opt} ${install_opt} ..
+    ninja
+    ninja install
 
+    echo "Leaving yaml"
     cd ../../
 }
 
-function install_tool() 
-{
+function install_tool() {
     ubuntu=$1
     centos=$2
     darwin=$3
-    
-    case "$system" in
-    "Linux") release=`awk -F= '/^NAME/{print $2}' /etc/os-release`
-        case "$release" in
-        "\"Ubuntu\"") 
-        apt-get -y install $ubuntu
-        ;;
-        "\"CentOS Linux\"")
-        yum install -y $centos
-        ;;
-        esac
-    ;;
 
-    "Darwin") 
-    brew install $darwin
-    ;;
+    case "$system" in
+    "Linux")
+        release=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
+        case "$release" in
+        "\"Ubuntu\"")
+            apt-get -y install $ubuntu
+            ;;
+        "\"CentOS Linux\"")
+            yum install -y $centos
+            ;;
+        esac
+        ;;
+
+    "Darwin")
+        brew install $darwin
+        ;;
 
     *)
-    echo "Unsupported System!"
-    echo "Please install library manually!"
-    ;;
+        echo "Unsupported System!"
+        echo "Please install library manually!"
+        ;;
     esac
 }
 
-function build_openssl()
-{  
-    echo "Installing openssl"
-    install_tool libssl-dev openssl-devel openssl
+function build_zlib() {
+    echo "Installing zlib"
+    if [ ! -d zlib ]; then
+        git clone ${git_url_prefix}madler/zlib.git
+    fi
+
+    cd zlib
+    rm -rf build
+    mkdir build
+    cd build
+    cmake -G Ninja ${compiler_opt} ${install_opt} ..
+    ninja
+    ninja install
+    echo "Leaving zlib "
+    cd ../../
 }
 
-function build_gtest()
-{
+function build_openssl() {
+    echo "Installing openssl (1.1.1)"
+    if [ $is_cross == "TRUE" ]; then
+        if [ ! -n "$cross_compiler_prefix" ]; then
+            echo "Error: cross_compiler_prefix must be set"
+            usage
+            return
+        fi
+
+        if [ ! -d openssl ]; then
+            git clone -b OpenSSL_1_1_1-stable ${git_url_prefix}openssl/openssl.git
+        fi
+
+        cd openssl
+        make clean
+        mkdir -p ${install_dir}/openssl/ssl
+        ./Configure ${system,}-${arch} no-asm shared \
+            --prefix=${install_dir}/openssl \
+            --openssldir=${install_dir}/openssl/ssl \
+            --cross-compile-prefix=${cross_compiler_prefix}- \
+            --with-zlib-include=${install_dir}/zlib/include \
+            --with-zlib-lib=${install_dir}/zlib/lib
+        make
+        make install
+        cd ../
+
+    else
+        install_tool libssl-dev openssl-devel openssl
+    fi
+}
+
+function build_gtest() {
     echo "Install gtest"
-    install_tool libgtest-dev gtest-devel googletest
+    if [ $is_cross != "TRUE" ]; then
+        install_tool libgtest-dev gtest-devel googletest
+    fi
 }
 
 function build_open62541() {
     echo "Install open62541 (v1.2.2)"
 
-    if [ ! -d open62541 ];then
-        git clone -b v1.2.2 https://github.com/open62541/open62541.git
+    if [ ! -d open62541 ]; then
+        git clone -b v1.2.2 ${git_url_prefix}open62541/open62541.git
     fi
-    cd open62541 
+    cd open62541
     rm -rf build
     mkdir build
     cd build
-    cmake -G Ninja -DBUILD_SHARED_LIBS=OFF -DUA_ENABLE_AMALGAMATION=ON -DUA_ENABLE_ENCRYPTION=ON -DUA_ENABLE_ENCRYPTION_OPENSSL=ON ..
+    cmake -G Ninja -DCMAKE_BUILD_TYPE=Release \
+        -DBUILD_SHARED_LIBS=OFF \
+        -DUA_ENABLE_AMALGAMATION=ON \
+        -DUA_ENABLE_ENCRYPTION=ON \
+        -DUA_ENABLE_ENCRYPTION_OPENSSL=ON \
+        ${ssl_lib_flag} ${compiler_opt} ${install_opt} ..
     ninja
     ninja install
 
@@ -172,37 +240,33 @@ function build_open62541() {
     cd ../../
 }
 
-current=`pwd`
+current=$(pwd)
 externs="/opt/externs"
 
-if [ ! -d $externs ];then
+if [ ! -d $externs ]; then
     mkdir $externs
 fi
-
-echo "Entering $externs"
 cd $externs
 
-ssl_lib_flag=""
-case "$system" in
-    "Linux") ssl_lib_flag=""
-    ;;
-    "Darwin") ssl_lib_flag="-DOPENSSL_ROOT_DIR=/usr/local/opt/openssl"
-    ;;
-    *)
-    ;;
-esac
-
-while getopts "a-:i:h" OPT; do
+while getopts "h:c-:r-:a-:n:p:s:" OPT; do
     case ${OPT} in
-    a) 
-        echo "Build ${list_str}"
-        for var in ${lib_list[*]};do
-            build_$var
-        done
+    c)
+        is_cross=TRUE
         ;;
-    i) 
+    s)
+        arch=$OPTARG
+        ;;
+    p)
+        cross_compiler_prefix=$OPTARG
+        ;;
+    r)
+        git_url_prefix="git@github.com:"
+        ;;
+    a)
+        is_build_all=TRUE
+        ;;
+    n)
         pkgname=$OPTARG
-        build_$pkgname
         ;;
     h)
         usage
@@ -215,6 +279,41 @@ while getopts "a-:i:h" OPT; do
     esac
 done
 
+if [ $is_cross == "TRUE" ]; then
+    install_dir=${externs}/libs/${cross_compiler_prefix}
+    compiler_opt="-DCMAKE_C_COMPILER=${cross_compiler_prefix}-gcc \
+        -DCMAKE_STAGING_PREFIX=${install_dir} -DCMAKE_PREFIX_PATH=${install_dir}"
+    install_opt="-DCMAKE_INSTALL_PREFIX=${install_dir}"
+    ssl_lib_flag="-DOPENSSL_ROOT_DIR=${install_dir}/openssl \
+        -DOPENSSL_INCLUDE_DIR==${install_dir}/openssl/include \
+        -DOPENSSL_LIBRARIES='${install_dir}/openssl/lib/libssl.so ${install_dir}/openssl/lib/libcrypto.so'"
+
+    echo "Entering $externs"
+else
+    case "$system" in
+    "Linux")
+        ssl_lib_flag=""
+        ;;
+    "Darwin")
+        ssl_lib_flag="-DOPENSSL_ROOT_DIR=/usr/local/opt/openssl"
+        ;;
+    *) ;;
+
+    esac
+fi
+
+mkdir -p ${install_dir}
+
+if [ $is_build_all == "TRUE" ]; then
+    echo "Build ${list_str}"
+    for var in ${lib_list[*]}; do
+        install_opt=${install_opt}/$var
+        build_$var
+    done
+else
+    install_opt=${install_opt}/$pkgname
+    build_$pkgname
+fi
+
 echo "Leaving $externs"
 cd $current
-
