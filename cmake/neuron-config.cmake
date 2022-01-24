f3b0b1403002ada56881543eb0327691b079fc17
gc87 <sfme@qq.com>
2022-01-24 13:53:38 +0800

    Modify the cmake prefix path

--- cmake/neuron-config.cmake
+++ cmake/neuron-config.cmake
@@ -1,6 +1,5 @@
-find_path(Neuron_INCLUDE_DIR neuron.h /usr/local/include/neuron)
-find_library(Neuron_LIBRARY NAMES neuron-base PATHS /usr/local/lib)
-
+find_path(Neuron_INCLUDE_DIR neuron.h ${CMAKE_PREFIX_PATH}/include/neuron)
+find_library(Neuron_LIBRARY NAMES neuron-base PATHS ${CMAKE_PREFIX_PATH}/lib)
 if (Neuron_INCLUDE_DIR AND Neuron_LIBRARY)
     set(Neuron_FOUND TRUE)
 endif()
