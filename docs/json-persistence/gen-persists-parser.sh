ed5af8c39b6deda1a9a7f9cf645189cda31972b4
Parker Liu <liuchangsheng30@gmail.com>
2022-02-18 11:18:17 +0800

    Add post process for generated persistance parser files

--- docs/json-persistence/gen-persists-parser.sh
+++ docs/json-persistence/gen-persists-parser.sh
@@ -2,10 +2,11 @@
 
 outdir=${outc_dir:-"."}
 
-json-autotype --clang --no-autounify --debug -t json_node ./json_adapter.json -o json_adapter.c
-json-autotype --clang --no-autounify --debug -t json_datatags ./json_datatags.json -o json_datatags.c
-json-autotype --clang --no-autounify --debug -t json_group_configs ./json_group_configs.json -o json_group_configs.c
-json-autotype --clang --no-autounify --debug -t json_plugin ./json_plugin.json -o json_plugin.c
-json-autotype --clang --no-autounify --debug -t json_subscriptions ./json_subs.json -o json_subs.c
+json-autotype --clang --no-autounify --debug -t json_node ./json_adapter.json -o persist_json_adapter.c
+json-autotype --clang --no-autounify --debug -t json_datatags ./json_datatags.json -o persist_json_datatags.c
+json-autotype --clang --no-autounify --debug -t json_group_configs ./json_group_configs.json -o persist_json_group_configs.c
+json-autotype --clang --no-autounify --debug -t json_plugin ./json_plugin.json -o persist_json_plugin.c
+json-autotype --clang --no-autounify --debug -t json_subscriptions ./json_subs.json -o persist_json_subs.c
 
+find -iname \*.c |xargs sed -i 's/json\/persist/persist/g'
 mv *.c *.h ${outdir}/
