b29c8b33e6cd63dfb74438233518c990eb1fb3eb
eeff <33566201+eeff@users.noreply.github.com>
2022-03-04 16:52:53 +0800

    Update neuron license key definition

--- include/neu_license.h
+++ include/neu_license.h
@@ -27,29 +27,30 @@
 // Type: string
 #define NEURON_LIC_KEY_LICENSE_TYPE "1.3.6.1.4.1.52509.10"
 
-// Max number of plugins.
-// Type: integer, non-negative
-#define NEURON_LIC_KEY_PLUGINS_NUM_MAX "1.3.6.1.4.1.52509.11"
-
 // Max number of nodes.
 // Type: integer, non-negative
-#define NEURON_LIC_KEY_NODES_NUM_MAX "1.3.6.1.4.1.52509.12"
+#define NEURON_LIC_KEY_NODES_NUM_MAX "1.3.6.1.4.1.52509.11"
 
 // Max number of tags globally.
 // Type: integer, non-negative
-#define NEURON_LIC_KEY_TAGS_NUM_MAX "1.3.6.1.4.1.52509.13"
+#define NEURON_LIC_KEY_TAGS_NUM_MAX "1.3.6.1.4.1.52509.12"
 
-// If modbus_plus_tcp plugin enabled.
-// Type: integer, 0 | 1
-#define NEURON_LIC_KEY_MODBUS_PLUS_TCP_ENABLED "1.3.6.1.4.1.52509.14"
+// Bit flag indicating which plugins are enabled.
+// Type: integer
+//
+//   MSB                                LSB
+//    63, 62, ...              3, 2, 1, 0
+//    ^----reserved------------^  ^  ^  ^
+//                                |  |  |
+//   opcua------------------------+  |  |
+//   modbus_rtu----------------------+  |
+//   modbus_tcp_plus--------------------+
+//
+#define NEURON_LIC_KEY_PLUGIN_FLAG "1.3.6.1.4.1.52509.13"
 
-// If modbus_rtu plugin enabled
-// Type: integer, 0 | 1
-#define NEURON_LIC_KEY_MODBUS_RTU_ENABLED "1.3.6.1.4.1.52509.15"
-
-// If opcua plugin enabled
-// Type: integer, 0 | 1
-#define NEURON_LIC_KEY_OPCUA_ENABLED "1.3.6.1.4.1.52509.16"
+#define NEURON_LIC_FLAG_MODBUS_TCP_PLUS 0x0000000000000001
+#define NEURON_LIC_FLAG_MODBUS_RTU 0x0000000000000002
+#define NEURON_LIC_FLAG_OPCUA 0x0000000000000004
 
 #ifdef __cplusplus
 extern "C" {
