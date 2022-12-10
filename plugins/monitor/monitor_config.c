d54125593843b167663ae2ade499e350989a01e3
eeff <33566201+eeff@users.noreply.github.com>
2022-12-10 12:24:52 +0800

    feat(monitor): add setting

---
+++ plugins/monitor/monitor_config.c
@@ -0,0 +1,258 @@
+/**
+ * NEURON IIoT System for Industry 4.0
+ * Copyright (C) 2020-2022 EMQ Technologies Co., Ltd All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ **/
+
+#include "json/json.h"
+#include "json/neu_json_param.h"
+
+#include "utils/base64.h"
+#include "utils/log.h"
+
+#include "monitor.h"
+#include "monitor_config.h"
+
+#define MB 1000000
+
+static inline int decode_b64_param(neu_plugin_t *plugin, neu_json_elem_t *el)
+{
+    int   len = 0;
+    char *s   = (char *) neu_decode64(&len, el->v.val_str);
+
+    if (NULL == s) {
+        plog_error(plugin, "setting %s invalid base64", el->name);
+        return -1;
+    }
+
+    if (0 == len) {
+        plog_error(plugin, "setting empty %s", el->name);
+        free(s);
+        return -1;
+    }
+
+    free(el->v.val_str);
+    el->v.val_str = s;
+    return 0;
+}
+
+static inline int parse_b64_param(neu_plugin_t *plugin, const char *setting,
+                                  neu_json_elem_t *el)
+{
+    if (0 != neu_parse_param(setting, NULL, 1, el)) {
+        plog_error(plugin, "setting no %s", el->name);
+        return -1;
+    }
+
+    if (0 != decode_b64_param(plugin, el)) {
+        free(el->v.val_str);
+        el->v.val_str = NULL;
+        return -1;
+    }
+
+    return 0;
+}
+
+static inline int parse_ssl_params(neu_plugin_t *plugin, const char *setting,
+                                   neu_json_elem_t *ssl, neu_json_elem_t *ca,
+                                   neu_json_elem_t *cert, neu_json_elem_t *key,
+                                   neu_json_elem_t *keypass)
+{
+    // ssl, optional
+    int ret = neu_parse_param(setting, NULL, 1, ssl);
+    if (0 != ret) {
+        plog_info(plugin, "setting no ssl");
+        return 0;
+    }
+
+    if (false == ssl->v.val_bool) {
+        plog_info(plugin, "setting ssl disabled");
+        return 0;
+    }
+
+    // ca, required if ssl enabled
+    ret = parse_b64_param(plugin, setting, ca);
+    if (0 != ret) {
+        return -1;
+    }
+
+    // cert, optional
+    ret = neu_parse_param(setting, NULL, 1, cert);
+    if (0 != ret) {
+        plog_info(plugin, "setting no cert");
+        return 0;
+    }
+
+    if (0 != decode_b64_param(plugin, cert)) {
+        return -1;
+    }
+
+    // key, required if cert enable
+    ret = parse_b64_param(plugin, setting, key);
+    if (0 != ret) {
+        return -1;
+    }
+
+    // keypass, optional
+    ret = neu_parse_param(setting, NULL, 1, keypass);
+    if (0 != ret) {
+        plog_info(plugin, "setting no keypass");
+    } else if (0 == strlen(keypass->v.val_str)) {
+        plog_error(plugin, "setting invalid keypass");
+        return -1;
+    }
+
+    return 0;
+}
+
+int monitor_config_parse(neu_plugin_t *plugin, const char *setting,
+                         monitor_config_t *config)
+{
+    int         ret         = 0;
+    char *      err_param   = NULL;
+    const char *placeholder = "********";
+
+    neu_json_elem_t client_id = { .name = "client-id", .t = NEU_JSON_STR };
+    neu_json_elem_t heartbeat_topic = { .name = "heartbeat-topic",
+                                        .t    = NEU_JSON_STR };
+    neu_json_elem_t host            = { .name = "host", .t = NEU_JSON_STR };
+    neu_json_elem_t port            = { .name = "port", .t = NEU_JSON_INT };
+    neu_json_elem_t username        = { .name = "username", .t = NEU_JSON_STR };
+    neu_json_elem_t password        = { .name = "password", .t = NEU_JSON_STR };
+    neu_json_elem_t ssl             = { .name = "ssl", .t = NEU_JSON_BOOL };
+    neu_json_elem_t ca              = { .name = "ca", .t = NEU_JSON_STR };
+    neu_json_elem_t cert            = { .name = "cert", .t = NEU_JSON_STR };
+    neu_json_elem_t key             = { .name = "key", .t = NEU_JSON_STR };
+    neu_json_elem_t keypass         = { .name = "keypass", .t = NEU_JSON_STR };
+
+    if (NULL == setting || NULL == config) {
+        plog_error(plugin, "invalid argument, null pointer");
+        return -1;
+    }
+
+    ret = neu_parse_param(setting, &err_param, 4, &client_id, &heartbeat_topic,
+                          &host, &port);
+    if (0 != ret) {
+        plog_error(plugin, "parsing setting fail, key: `%s`", err_param);
+        goto error;
+    }
+
+    // client-id, required
+    if (0 == strlen(client_id.v.val_str)) {
+        plog_error(plugin, "setting empty client-id");
+        goto error;
+    }
+
+    // heartbeat-topic, required
+    if (0 == strlen(heartbeat_topic.v.val_str)) {
+        plog_error(plugin, "setting empty heartbeat-topic");
+        goto error;
+    }
+
+    // host, required
+    if (0 == strlen(host.v.val_str)) {
+        plog_error(plugin, "setting invalid host: `%s`", host.v.val_str);
+        goto error;
+    }
+
+    // port, required
+    if (0 == port.v.val_int || port.v.val_int > 65535) {
+        plog_error(plugin, "setting invalid port: %" PRIi64, port.v.val_int);
+        goto error;
+    }
+
+    // username, optional
+    ret = neu_parse_param(setting, NULL, 1, &username);
+    if (0 != ret) {
+        plog_info(plugin, "setting no username");
+    }
+
+    // password, optional
+    ret = neu_parse_param(setting, NULL, 1, &password);
+    if (0 != ret) {
+        plog_info(plugin, "setting no password");
+    }
+
+    ret = parse_ssl_params(plugin, setting, &ssl, &ca, &cert, &key, &keypass);
+    if (0 != ret) {
+        goto error;
+    }
+
+    config->client_id       = client_id.v.val_str;
+    config->heartbeat_topic = heartbeat_topic.v.val_str;
+    config->host            = host.v.val_str;
+    config->port            = port.v.val_int;
+    config->username        = username.v.val_str;
+    config->password        = password.v.val_str;
+    config->ca              = ca.v.val_str;
+    config->cert            = cert.v.val_str;
+    config->key             = key.v.val_str;
+    config->keypass         = keypass.v.val_str;
+
+    plog_info(plugin, "config client-id      : %s", config->client_id);
+    plog_info(plugin, "config heartbeat-topic: %s", config->heartbeat_topic);
+    plog_info(plugin, "config host           : %s", config->host);
+    plog_info(plugin, "config port           : %" PRIu16, config->port);
+
+    if (config->username) {
+        plog_info(plugin, "config username       : %s", config->username);
+    }
+    if (config->password) {
+        plog_info(plugin, "config password       : %s",
+                  0 == strlen(config->password) ? "" : placeholder);
+    }
+    if (config->ca) {
+        plog_info(plugin, "config ca             : %s", placeholder);
+    }
+    if (config->cert) {
+        plog_info(plugin, "config cert           : %s", placeholder);
+    }
+    if (config->key) {
+        plog_info(plugin, "config key            : %s", placeholder);
+    }
+    if (config->keypass) {
+        plog_info(plugin, "config keypass        : %s", placeholder);
+    }
+
+    return 0;
+
+error:
+    free(err_param);
+    free(heartbeat_topic.v.val_str);
+    free(host.v.val_str);
+    free(username.v.val_str);
+    free(password.v.val_str);
+    free(ca.v.val_str);
+    free(cert.v.val_str);
+    free(key.v.val_str);
+    free(keypass.v.val_str);
+    return -1;
+}
+
+void monitor_config_fini(monitor_config_t *config)
+{
+    free(config->client_id);
+    free(config->heartbeat_topic);
+    free(config->host);
+    free(config->username);
+    free(config->password);
+    free(config->ca);
+    free(config->cert);
+    free(config->key);
+    free(config->keypass);
+
+    memset(config, 0, sizeof(*config));
+}
---
+++ plugins/monitor/monitor_config.h
@@ -0,0 +1,53 @@
+/**
+ * NEURON IIoT System for Industry 4.0
+ * Copyright (C) 2020-2022 EMQ Technologies Co., Ltd All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ **/
+
+#ifndef NEURON_PLUGIN_MONITOR_CONFIG_H
+#define NEURON_PLUGIN_MONITOR_CONFIG_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "plugin.h"
+
+typedef struct {
+    char *   client_id;       // client id
+    char *   heartbeat_topic; // heartbeat topic
+    char *   host;            // broker host
+    uint16_t port;            // broker port
+    char *   username;        // user name
+    char *   password;        // user password
+    char *   ca;              // CA
+    char *   cert;            // client cert
+    char *   key;             // client key
+    char *   keypass;         // client key password
+} monitor_config_t;
+
+int  monitor_config_parse(neu_plugin_t *plugin, const char *setting,
+                          monitor_config_t *config);
+void monitor_config_fini(monitor_config_t *config);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
