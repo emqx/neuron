0295ea0b8ff9e6f3c06970ac52af7f557956eff0
fengzero <fengzero@fengzero.com>
2022-12-22 08:19:16 +0000

    update ci

--- cmake/arm-linux-gnueabi.cmake
+++ cmake/arm-linux-gnueabi.cmake
@@ -1,7 +1,7 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(COMPILER_PREFIX arm-linux-gnueabi)
 set(CMAKE_SYSTEM_PROCESSOR armv5tejl)
-set(LIBRARY_DIR /opt/externs/libs)
+set(LIBRARY_DIR /home/neuron/libs)
 
 set(CMAKE_C_COMPILER ${COMPILER_PREFIX}-gcc)
 set(CMAKE_CXX_COMPILER ${COMPILER_PREFIX}-g++)
