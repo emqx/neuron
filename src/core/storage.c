a3451b77656a47ce4b647d2ea8790bb466935707
eeff <33566201+eeff@users.noreply.github.com>
2023-01-10 23:30:38 +0800

    fix(core): send NEU_REQ_SUBSCRIBE_GROUP when loading subscriptions

--- src/core/storage.c
+++ src/core/storage.c
@@ -154,6 +154,11 @@ int manager_load_subscribe(neu_manager_t *manager)
                 nlog_info("%s load subscription app:%s driver:%s grp:%s",
                           ok_or_err, node->node, info->driver_name,
                           info->group_name);
+                if (0 == rv) {
+                    neu_manager_send_subscribe(manager, node->node,
+                                               info->driver_name,
+                                               info->group_name, info->params);
+                }
             }
 
             utarray_free(sub_infos);
