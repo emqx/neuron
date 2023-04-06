bc77bd13a3480469f207e5f930c40055d0f940a9
hxy7yx <1595670487@qq.com>
2023-04-06 15:33:38 +0800

    Open source Modbus TCP/RTU, delete existing Modbus TCP Community

--- plugins/modbus/modbus_req.c
+++ plugins/modbus/modbus_req.c
@@ -1,29 +1,13 @@
-/**
- * NEURON IIoT System for Industry 4.0
- * Copyright (C) 2020-2022 EMQ Technologies Co., Ltd All rights reserved.
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- **/
+#include <time.h>
+
 #include "modbus_point.h"
 #include "modbus_stack.h"
 
 #include "modbus_req.h"
 
 struct modbus_group_data {
-    char *                  group;
     UT_array *              tags;
+    char *                  group;
     modbus_read_cmd_sort_t *cmd_sort;
 };
 
@@ -46,22 +30,76 @@ void modbus_conn_disconnected(void *data, int fd)
     plugin->common.link_state = NEU_NODE_LINK_STATE_DISCONNECTED;
 }
 
+void modbus_tcp_server_listen(void *data, int fd)
+{
+    struct neu_plugin *  plugin = (struct neu_plugin *) data;
+    neu_event_io_param_t param  = {
+        .cb       = modbus_tcp_server_io_callback,
+        .fd       = fd,
+        .usr_data = (void *) plugin,
+    };
+
+    plugin->tcp_server_io = neu_event_add_io(plugin->events, param);
+}
+
+void modbus_tcp_server_stop(void *data, int fd)
+{
+    struct neu_plugin *plugin = (struct neu_plugin *) data;
+    (void) fd;
+
+    neu_event_del_io(plugin->events, plugin->tcp_server_io);
+}
+
+int modbus_tcp_server_io_callback(enum neu_event_io_type type, int fd,
+                                  void *usr_data)
+{
+    neu_plugin_t *plugin = (neu_plugin_t *) usr_data;
+
+    switch (type) {
+    case NEU_EVENT_IO_READ: {
+        int client_fd = neu_conn_tcp_server_accept(plugin->conn);
+        if (client_fd > 0) {
+            plugin->client_fd = client_fd;
+        }
+
+        break;
+    }
+    case NEU_EVENT_IO_CLOSED:
+    case NEU_EVENT_IO_HUP:
+        plog_warn(plugin, "tcp server recv: %d, conn closed, fd: %d", type, fd);
+        neu_event_del_io(plugin->events, plugin->tcp_server_io);
+        neu_conn_disconnect(plugin->conn);
+        break;
+    }
+
+    return 0;
+}
+
 int modbus_send_msg(void *ctx, uint16_t n_byte, uint8_t *bytes)
 {
     neu_plugin_t *plugin = (neu_plugin_t *) ctx;
+    int           ret    = 0;
 
     plog_send_protocol(plugin, bytes, n_byte);
-    return neu_conn_send(plugin->conn, bytes, n_byte);
+
+    if (plugin->is_server) {
+        ret = neu_conn_tcp_server_send(plugin->conn, plugin->client_fd, bytes,
+                                       n_byte);
+    } else {
+        ret = neu_conn_send(plugin->conn, bytes, n_byte);
+    }
+
+    return ret;
 }
 
 int modbus_group_timer(neu_plugin_t *plugin, neu_plugin_group_t *group,
                        uint16_t max_byte)
 {
+    neu_conn_state_t               state = { 0 };
     neu_adapter_update_metric_cb_t update_metric =
         plugin->common.adapter_callbacks->update_metric;
-    struct modbus_group_data *gd    = NULL;
-    neu_conn_state_t          state = { 0 };
-    int64_t                   rtt   = NEU_METRIC_LAST_RTT_MS_MAX;
+    struct modbus_group_data *gd  = NULL;
+    int64_t                   rtt = NEU_METRIC_LAST_RTT_MS_MAX;
 
     if (group->user_data == NULL) {
         gd = calloc(1, sizeof(struct modbus_group_data));
@@ -81,32 +119,39 @@ int modbus_group_timer(neu_plugin_t *plugin, neu_plugin_group_t *group,
 
         gd->group    = strdup(group->group_name);
         gd->cmd_sort = modbus_tag_sort(gd->tags, max_byte);
-    } else {
-        gd = (struct modbus_group_data *) group->user_data;
     }
+
+    gd                        = (struct modbus_group_data *) group->user_data;
     plugin->plugin_group_data = gd;
 
     for (uint16_t i = 0; i < gd->cmd_sort->n_cmd; i++) {
-        uint16_t response_size = 0;
-        int64_t  start         = neu_time_ms();
         plugin->cmd_idx        = i;
+        uint16_t response_size = 0;
+        uint64_t read_tms      = neu_time_ms();
 
         int ret = modbus_stack_read(
             plugin->stack, gd->cmd_sort->cmd[i].slave_id,
             gd->cmd_sort->cmd[i].area, gd->cmd_sort->cmd[i].start_address,
             gd->cmd_sort->cmd[i].n_register, &response_size);
         if (ret > 0) {
-            process_protocol_buf(plugin, response_size);
-
-            int64_t end = neu_time_ms();
-            if (end - start < rtt) {
-                rtt = end - start;
-            }
+            ret = process_protocol_buf(plugin, response_size);
+            rtt = neu_time_ms() - read_tms;
+        }
+        if (ret <= 0) {
+            modbus_value_handle(plugin, gd->cmd_sort->cmd[i].slave_id, 0, NULL,
+                                NEU_ERR_PLUGIN_DISCONNECTED);
+            rtt = NEU_METRIC_LAST_RTT_MS_MAX;
+        }
+        if (plugin->interval > 0) {
+            struct timespec t1 = { .tv_sec  = plugin->interval / 1000,
+                                   .tv_nsec = 1000 * 1000 *
+                                       (plugin->interval % 1000) };
+            struct timespec t2 = { 0 };
+            nanosleep(&t1, &t2);
         }
     }
 
     state = neu_conn_state(plugin->conn);
-
     update_metric(plugin->common.adapter, NEU_METRIC_SEND_BYTES,
                   state.send_bytes, NULL);
     update_metric(plugin->common.adapter, NEU_METRIC_RECV_BYTES,
@@ -114,12 +159,11 @@ int modbus_group_timer(neu_plugin_t *plugin, neu_plugin_group_t *group,
     update_metric(plugin->common.adapter, NEU_METRIC_LAST_RTT_MS, rtt, NULL);
     update_metric(plugin->common.adapter, NEU_METRIC_GROUP_LAST_SEND_MSGS,
                   gd->cmd_sort->n_cmd, group->group_name);
-
     return 0;
 }
 
 int modbus_value_handle(void *ctx, uint8_t slave_id, uint16_t n_byte,
-                        uint8_t *bytes)
+                        uint8_t *bytes, int error)
 {
     neu_plugin_t *            plugin = (neu_plugin_t *) ctx;
     struct modbus_group_data *gd =
@@ -127,11 +171,11 @@ int modbus_value_handle(void *ctx, uint8_t slave_id, uint16_t n_byte,
     uint16_t start_address = gd->cmd_sort->cmd[plugin->cmd_idx].start_address;
     uint16_t n_register    = gd->cmd_sort->cmd[plugin->cmd_idx].n_register;
 
-    if (bytes == NULL) {
+    if (error != NEU_ERR_SUCCESS) {
         neu_dvalue_t dvalue = { 0 };
 
         dvalue.type      = NEU_TYPE_ERROR;
-        dvalue.value.i32 = NEU_ERR_PLUGIN_DISCONNECTED;
+        dvalue.value.i32 = error;
         plugin->common.adapter_callbacks->driver.update(
             plugin->common.adapter, gd->group, NULL, dvalue);
         return 0;
@@ -182,6 +226,11 @@ int modbus_value_handle(void *ctx, uint8_t slave_id, uint16_t n_byte,
             case NEU_TYPE_UINT32:
                 dvalue.value.u32 = ntohl(dvalue.value.u32);
                 break;
+            case NEU_TYPE_DOUBLE:
+            case NEU_TYPE_INT64:
+            case NEU_TYPE_UINT64:
+                dvalue.value.u64 = neu_ntohll(dvalue.value.u64);
+                break;
             case NEU_TYPE_BIT: {
                 switch ((*p_tag)->area) {
                 case MODBUS_AREA_HOLD_REGISTER:
@@ -196,7 +245,6 @@ int modbus_value_handle(void *ctx, uint8_t slave_id, uint16_t n_byte,
                 case MODBUS_AREA_INPUT:
                     break;
                 }
-
                 break;
             }
             case NEU_TYPE_STRING: {
@@ -218,7 +266,6 @@ int modbus_value_handle(void *ctx, uint8_t slave_id, uint16_t n_byte,
                     dvalue.value.str[0] = '?';
                     dvalue.value.str[1] = 0;
                 }
-
                 break;
             }
             default:
@@ -242,18 +289,23 @@ int modbus_write(neu_plugin_t *plugin, void *req, neu_datatag_t *tag,
 
     switch (tag->type) {
     case NEU_TYPE_UINT16:
-    case NEU_TYPE_INT16: {
+    case NEU_TYPE_INT16:
         value.u16 = htons(value.u16);
         n_byte    = sizeof(uint16_t);
         break;
-    }
+    case NEU_TYPE_FLOAT:
     case NEU_TYPE_UINT32:
     case NEU_TYPE_INT32:
-    case NEU_TYPE_FLOAT: {
         value.u32 = htonl(value.u32);
         n_byte    = sizeof(uint32_t);
         break;
-    }
+
+    case NEU_TYPE_DOUBLE:
+    case NEU_TYPE_INT64:
+    case NEU_TYPE_UINT64:
+        value.u64 = neu_htonll(value.u64);
+        n_byte    = sizeof(uint64_t);
+        break;
     case NEU_TYPE_BIT: {
         n_byte = sizeof(uint8_t);
         break;
@@ -318,12 +370,21 @@ static int process_protocol_buf(neu_plugin_t *plugin, uint16_t response_size)
     neu_protocol_unpack_buf_t pbuf     = { 0 };
     ssize_t                   ret      = 0;
 
-    ret = neu_conn_recv(plugin->conn, recv_buf, response_size);
+    if (plugin->is_server) {
+        ret = neu_conn_tcp_server_recv(plugin->conn, plugin->client_fd,
+                                       recv_buf, response_size);
+    } else {
+        ret = neu_conn_recv(plugin->conn, recv_buf, response_size);
+    }
 
-    if (ret > 0) {
+    if (ret == response_size) {
+        if (response_size < 512) {
+            plog_recv_protocol(plugin, recv_buf, ret);
+        }
         neu_protocol_unpack_buf_init(&pbuf, recv_buf, ret);
-        plog_recv_protocol(plugin, recv_buf, ret);
         ret = modbus_stack_recv(plugin->stack, &pbuf);
+    } else {
+        ret = -1;
     }
 
     free(recv_buf);
