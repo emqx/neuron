30d6bc440763527585258ace5b8ce6fd9095b44e
eeff <33566201+eeff@users.noreply.github.com>
2023-04-14 18:16:11 +0800

    feat(restful): add template get tags api

--- plugins/restful/template_handle.h
+++ plugins/restful/template_handle.h
@@ -33,5 +33,6 @@ void handle_get_templates_resp(nng_aio *aio, neu_resp_get_templates_t *resp);
 void handle_add_template_tags(nng_aio *aio);
 void handle_update_template_tags(nng_aio *aio);
 void handle_del_template_tags(nng_aio *aio);
+void handle_get_template_tags(nng_aio *aio);
 
 #endif
