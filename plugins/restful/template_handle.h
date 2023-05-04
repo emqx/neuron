aa4e94cc6daec01066b841118c3a7b975e598403
eeff <33566201+eeff@users.noreply.github.com>
2023-05-04 11:07:28 +0800

    feat(restful): add template group del api

--- plugins/restful/template_handle.h
+++ plugins/restful/template_handle.h
@@ -32,6 +32,7 @@ void handle_get_templates_resp(nng_aio *aio, neu_resp_get_templates_t *resp);
 
 void handle_add_template_group(nng_aio *aio);
 void handle_update_template_group(nng_aio *aio);
+void handle_del_template_group(nng_aio *aio);
 
 void handle_add_template_tags(nng_aio *aio);
 void handle_update_template_tags(nng_aio *aio);
