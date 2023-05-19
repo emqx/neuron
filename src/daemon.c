c88b60c8c3951620c4d2e150a2e2df30eed56b02
fengzero <fengzero@fengzero.com>
2023-05-19 07:58:47 +0000

    fix(daemon): log format

--- src/daemon.c
+++ src/daemon.c
@@ -129,7 +129,7 @@ int neuron_already_running()
 
     snprintf(buf, sizeof(buf), "%ld", (long) getpid());
     size = write(fd, buf, strlen(buf) + 1);
-    nlog_warn("write %s ret: %d, %s(%d)", NEURON_DAEMON_LOCK_FNAME, size,
+    nlog_warn("write %s, error %s(%d)", NEURON_DAEMON_LOCK_FNAME,
               strerror(errno), errno);
     assert(size != -1);
 
