f31a7fc2c3c4013a0114e608c6e8f3e2fddf8887
fengzero <fengzero@fengzero.com>
2023-10-27 08:29:31 +0000

    drive: default interval 100ms

--- src/adapter/driver/driver.c
+++ src/adapter/driver/driver.c
@@ -1206,8 +1206,8 @@ int neu_adapter_driver_add_tag(neu_adapter_driver_t *driver, const char *group,
 
     HASH_FIND_STR(driver->groups, group, find);
     if (find == NULL) {
-        neu_adapter_driver_add_group(driver, group, 300);
-        adapter_storage_add_group(driver->adapter.name, group, 300);
+        neu_adapter_driver_add_group(driver, group, 100);
+        adapter_storage_add_group(driver->adapter.name, group, 100);
     }
     HASH_FIND_STR(driver->groups, group, find);
     assert(find != NULL);
