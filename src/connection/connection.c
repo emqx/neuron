465fff7710ef97fe0273cf122596260fd740675d
fengzero <fengzero@fengzero.com>
2023-11-01 08:48:09 +0000

    chore(conn): disconnect if udp recv error

--- src/connection/connection.c
+++ src/connection/connection.c
@@ -447,11 +447,21 @@ ssize_t neu_conn_recv(neu_conn_t *conn, uint8_t *buf, ssize_t len)
                 conn->fd, len, ret, strerror(errno), errno);
             conn_disconnect(conn);
         }
+    } else if (conn->param.type == NEU_CONN_UDP ||
+               conn->param.type == NEU_CONN_UDP_TO) {
+        if (ret <= 0) {
+            zlog_error(
+                conn->param.log,
+                "udp conn fd: %d, recv buf len %zd, ret: %zd, errno: %s(%d)",
+                conn->fd, len, ret, strerror(errno), errno);
+            conn_disconnect(conn);
+        }
     } else {
         if (ret <= 0) {
-            zlog_error(conn->param.log,
-                       "conn fd: %d, recv buf len %zd, ret: %zd, errno: %s(%d)",
-                       conn->fd, len, ret, strerror(errno), errno);
+            zlog_error(
+                conn->param.log,
+                "tcp conn fd: %d, recv buf len %zd, ret: %zd, errno: %s(%d)",
+                conn->fd, len, ret, strerror(errno), errno);
             if (ret == 0 || (ret == -1 && errno != EAGAIN)) {
                 conn_disconnect(conn);
             }
