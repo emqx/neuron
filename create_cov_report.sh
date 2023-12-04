effd34e08fc66a8f3aa4708c3a943ae4c60aa8b2
eeff <33566201+eeff@users.noreply.github.com>
2023-12-04 21:46:11 +0800

    test: add mqtt ft

--- create_cov_report.sh
+++ create_cov_report.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
+# core trace
+lcov -c -d build/CMakeFiles/neuron-base.dir/src -o cov_report/cov-neuron-base.info
+lcov -c -d build/CMakeFiles/neuron.dir/src -o cov_report/cov-neuron.info
+lcov -c -d build/CMakeFiles/neuron.dir/plugins/restful -o cov_report/cov-restful.info
+
+# always with core trace
+function gen_trace () {
+    lcov -a cov-neuron-base.info -a cov-neuron.info -a cov-restful.info $@;
+}
 
 case $1 in
     (modbus)
         lcov -c -d build/plugins/modbus/CMakeFiles/plugin-modbus-rtu.dir -o cov_report/cov-modbus-rtu.info
         lcov -c -d build/plugins/modbus/CMakeFiles/plugin-modbus-tcp.dir -o cov_report/cov-modbus-tcp.info
-        lcov -c -d build/CMakeFiles/neuron-base.dir/src -o cov_report/cov-neuron-base.info
-        lcov -c -d build/CMakeFiles/neuron.dir/src -o cov_report/cov-neuron.info
-        lcov -c -d build/CMakeFiles/neuron.dir/plugins/restful -o cov_report/cov-restful.info
         cd cov_report
-        lcov -a cov-neuron-base.info -a cov-neuron.info -a cov-restful.info -a cov-modbus-rtu.info -a cov-modbus-tcp.info -o cov-modbus.info;;
+        gen_trace -a cov-modbus-rtu.info -a cov-modbus-tcp.info -o cov-modbus.info;;
+    (mqtt)
+        lcov -c -d build/plugins/mqtt/CMakeFiles/plugin-mqtt.dir -o cov_report/cov-mqtt.info
+        cd cov_report
+        gen_trace -a cov-mqtt.info -o cov-mqtt.info;;
     (core)
-        lcov -c -d build/CMakeFiles/neuron-base.dir/src -o cov_report/cov-neuron-base.info
-        lcov -c -d build/CMakeFiles/neuron.dir/src -o cov_report/cov-neuron.info
-        lcov -c -d build/CMakeFiles/neuron.dir/plugins/restful -o cov_report/cov-restful.info
         cd cov_report
-        lcov -a cov-neuron-base.info -a cov-neuron.info -a cov-restful.info -o cov-core.info;;
+        gen_trace -o cov-core.info;;
     (metrics)
-        lcov -c -d build/CMakeFiles/neuron-base.dir/src -o cov_report/cov-neuron-base.info
-        lcov -c -d build/CMakeFiles/neuron.dir/src -o cov_report/cov-neuron.info
-        lcov -c -d build/CMakeFiles/neuron.dir/plugins/restful -o cov_report/cov-restful.info
         cd cov_report
-        lcov -a cov-neuron-base.info -a cov-neuron.info -a cov-restful.info -o cov-metrics.info;;
-        (*)
+        gen_trace -o cov-metrics.info;;
+    (*)
         lcov -c -d build/plugins/$1/CMakeFiles/plugin-$1.dir -o cov_report/cov-$1.info;;
 esac
