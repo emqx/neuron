89f7e237a2d043f9207fb4d26ffd838e4c9101fe
fengzero <fengzero@fengzero.com>
2023-12-11 10:21:46 +0000

    fix write tag in arm64

--- src/adapter/driver/driver.c
+++ src/adapter/driver/driver.c
@@ -585,9 +585,9 @@ static void cal_decimal(neu_type_e tag_type, neu_type_e value_type,
     case NEU_TYPE_LWORD:
     case NEU_TYPE_DWORD:
         if (value_type == NEU_TYPE_INT64 || value_type == NEU_TYPE_LWORD) {
-            value->u64 = value->u64 / decimal;
+            value->i64 = value->i64 / decimal;
         } else if (value_type == NEU_TYPE_DOUBLE) {
-            value->u64 = (uint64_t) round(value->d64 / decimal);
+            value->i64 = (int64_t) round(value->d64 / decimal);
         }
         break;
     case NEU_TYPE_FLOAT:
