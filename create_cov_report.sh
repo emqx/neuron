d293f38db94eca994c11d6eee79b924d7de3030a
hxy7yx <1595670487@qq.com>
2024-01-22 15:36:09 +0800

    adjusting ut coverage

--- create_cov_report.sh
+++ create_cov_report.sh
@@ -11,6 +11,21 @@ function gen_trace () {
 }
 
 case $1 in
+    (ut)
+        lcov -c -d build/tests/ut/CMakeFiles/tag_sort_test.dir -o cov_report/cov-tag_sort_test.info
+        lcov -c -d build/tests/ut/CMakeFiles/modbus_test.dir -o cov_report/cov-modbus_test.info
+        lcov -c -d build/tests/ut/CMakeFiles/jwt_test.dir -o cov_report/cov-jwt_test.info
+        lcov -c -d build/tests/ut/CMakeFiles/json_test.dir -o cov_report/cov-json_test.info
+        lcov -c -d build/tests/ut/CMakeFiles/http_test.dir -o cov_report/cov-http_test.info
+        lcov -c -d build/tests/ut/CMakeFiles/base64_test.dir -o cov_report/cov-base64_test.info
+        cd cov_report
+        lcov -a cov-tag_sort_test.info -a cov-modbus_test.info -a cov-jwt_test.info -a cov-json_test.info -a cov-http_test.info -a cov-base64_test.info -o cov-ut.info;;
+    (ft)
+        lcov -c -d build/plugins/mqtt/CMakeFiles/plugin-mqtt.dir -o cov_report/cov-mqtt.info
+        lcov -c -d build/plugins/modbus/CMakeFiles/plugin-modbus-rtu.dir -o cov_report/cov-modbus-rtu.info
+        lcov -c -d build/plugins/modbus/CMakeFiles/plugin-modbus-tcp.dir -o cov_report/cov-modbus-tcp.info
+        cd cov_report
+        gen_trace -a cov-mqtt.info -a cov-modbus-rtu.info -a cov-modbus-tcp.info -o cov-ft.info;;
     (ekuiper)
         lcov -c -d build/plugins/ekuiper/CMakeFiles/plugin-ekuiper.dir -o cov_report/cov-ekuiper.info
         cd cov_report
