87718d57f1cbbd133c5c4a86194363f78c14d0d4
hxy7yx <1595670487@qq.com>
2024-04-15 10:37:50 +0800

    Revert "check if the written value conforms to the standard"
    This reverts commit d9157687ede43cbe134416f0bd3045eac48ae0a6.

--- src/utils/http.c
+++ src/utils/http.c
@@ -320,7 +320,6 @@ int neu_http_response(nng_aio *aio, neu_err_code_e code, char *content)
     case NEU_ERR_TAG_PRECISION_INVALID:
     case NEU_ERR_TAG_NAME_TOO_LONG:
     case NEU_ERR_TAG_ADDRESS_TOO_LONG:
-    case NEU_ERR_TAG_VALUE_INVALID:
     case NEU_ERR_GROUP_MAX_GROUPS:
     case NEU_ERR_LICENSE_MAX_TAGS:
     case NEU_ERR_LICENSE_BAD_CLOCK:
