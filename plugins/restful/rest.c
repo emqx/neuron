f07e614ce647755adbdff6ff5a158c27d5d26835
hxy7yx <1595670487@qq.com>
2024-06-03 16:03:26 +0800

    feat(read):support pagination and filter(error) when reading tags

--- plugins/restful/rest.c
+++ plugins/restful/rest.c
@@ -228,6 +228,10 @@ static int dashb_plugin_request(neu_plugin_t *      plugin,
     case NEU_RESP_READ_GROUP:
         handle_read_resp(header->ctx, (neu_resp_read_group_t *) data);
         break;
+    case NEU_RESP_READ_GROUP_PAGINATE:
+        handle_read_paginate_resp(header->ctx,
+                                  (neu_resp_read_group_paginate_t *) data);
+        break;
     case NEU_RESP_GET_TEMPLATE:
         handle_get_template_resp(header->ctx, (neu_resp_get_template_t *) data);
         break;
