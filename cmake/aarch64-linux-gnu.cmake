ca2d8af6e840788dad02f4abad7fe413fd1c6102
fengzero <fengzero@fengzero.com>
2026-01-13 03:38:08 +0000

    branch v2.14

--- cmake/aarch64-linux-gnu.cmake
+++ cmake/aarch64-linux-gnu.cmake
@@ -1,7 +1,7 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(COMPILER_PREFIX aarch64-linux-gnu)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
-set(LIBRARY_DIR /home/neuron/main/libs)
+set(LIBRARY_DIR /home/neuron/v2.14/libs)
 
 set(CMAKE_C_COMPILER ${COMPILER_PREFIX}-gcc)
 set(CMAKE_CXX_COMPILER ${COMPILER_PREFIX}-g++)
