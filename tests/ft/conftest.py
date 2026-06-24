835b7b7fa3d3e79aa3479b994c1ac0bba31f3a59
hxy7yx <1595670487@qq.com>
2026-06-23 20:46:41 -0700

    fix(ft):mqtt configuration

--- tests/ft/conftest.py
+++ tests/ft/conftest.py
@@ -1,4 +1,10 @@
+import inspect
+
 import pytest
+
+import neuron.api as api
+import neuron.config as config
+import neuron.process as process
 
 
 def pytest_addoption(parser):
@@ -10,3 +16,37 @@ def pytest_addoption(parser):
 @pytest.fixture(autouse=True, scope="session")
 def get_envx(request):
     return request.config.getoption("--env")
+
+
+def _refresh_default_jwt(token):
+    old_jwt = config.default_jwt
+    new_jwt = f"Bearer {token}"
+
+    config.default_jwt = new_jwt
+
+    for _, func in inspect.getmembers(api, inspect.isfunction):
+        defaults = func.__defaults__
+        if defaults:
+            func.__defaults__ = tuple(
+                new_jwt if value == old_jwt else value for value in defaults
+            )
+
+        kwdefaults = func.__kwdefaults__
+        if kwdefaults:
+            func.__kwdefaults__ = {
+                key: new_jwt if value == old_jwt else value
+                for key, value in kwdefaults.items()
+            }
+
+
+@pytest.fixture(autouse=True, scope="session")
+def init_default_jwt():
+    process.remove_persistence()
+    neuron = process.NeuronProcess()
+    neuron.start()
+
+    response = api.login()
+    assert 200 == response.status_code
+    _refresh_default_jwt(response.json()["token"])
+
+    neuron.stop()
